82c61feac42a111d5a3610f8e57041db53f2473e
DanielBeeck <daniel.beck99@outlook.de>
2025-04-29 20:59:01 +0200

    implemented button with function: watch decrypted traffic via wireshark

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import subprocess
+import threading
 from flask import Flask, render_template, Response
 
 app = Flask(__name__)
@@ -9,13 +10,22 @@ def home():
 
 def stream_logs():
     process = subprocess.Popen(
-        ["docker", "logs", "-f", "polarproxy"],
+        ["docker", "logs", "-f", "polarproxy-pcap"],
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         text=True
     )
-    for line in process.stdout:
-        yield f"data: {line}\n\n"
+    if process.stdout is not None:
+        for line in process.stdout:
+            yield f"data: {line}\n\n"
+
+
+def run_polarproxy_with_wireshark():
+    cmd = (
+        "wireshark -k -i TCP@127.0.0.1:57012"
+    )
+    subprocess.run(cmd, shell=True)
+
 
 @app.route("/logs/stream")
 def log_stream():
@@ -24,6 +34,11 @@ def log_stream():
 @app.route("/logs")
 def logs():
         return render_template("logs.html")
+
+@app.route("/start-proxy")
+def start_proxy():
+    threading.Thread(target=run_polarproxy_with_wireshark, daemon=True).start()
+    return "PolarProxy mit Wireshark gestartet"
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
