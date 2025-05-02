fc518dab6293fe0f77c8f5ad86c6a2a258aa00a9
DanielBeeck <daniel.beck99@outlook.de>
2025-05-02 22:22:45 +0200

    added options to start/stop Polarproxy Container

--- app/app.py
+++ app/app.py
@@ -26,6 +26,20 @@ def run_polarproxy_with_wireshark():
     )
     subprocess.run(cmd, shell=True)
 
+def start_polarproxy():
+    cmd = (
+        "docker start polarproxy-pcap"
+    )
+    subprocess.run(cmd, shell=True,capture_output=True,
+            text=True)
+
+def stop_polarproxy():
+    cmd = (
+        "docker stop polarproxy-pcap"
+    )
+    subprocess.run(cmd, shell=True,capture_output=True,
+            text=True)
+
 
 @app.route("/logs/stream")
 def log_stream():
@@ -35,10 +49,24 @@ def log_stream():
 def logs():
         return render_template("logs.html")
 
-@app.route("/start-proxy")
-def start_proxy():
+@app.route("/options")
+def options():
+        return render_template("options.html")
+
+@app.route("/wireshark")
+def start_wireshark():
     threading.Thread(target=run_polarproxy_with_wireshark, daemon=True).start()
     return "PolarProxy mit Wireshark gestartet"
+
+@app.route("/start-proxy")
+def start_proxy():
+    threading.Thread(target=start_polarproxy, daemon=True).start()
+    return "PolarProxy gestartet"
+
+@app.route("/stop-proxy")
+def stop_proxy():
+    threading.Thread(target=stop_polarproxy, daemon=True).start()
+    return "PolarProxy gestoppt"
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
